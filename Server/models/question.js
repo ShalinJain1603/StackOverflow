90eb20e510c8a7d8cb4fcb9964e38ec81c5dca1f
partthdhage <parthdhage24@gmail.com>
2021-08-12 15:46:15 +0530

    Schema Correction

---
+++ Server/models/question.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+
+const questionSchema = new mongoose.Schema({
+  author: 
+     { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User' 
+      },
+  
+    title: {
+      required : true,
+      type: String,
+      maxLength : 100,
+    },
+  text: {
+    required : true,
+    type: String,
+  },
+  tags: {
+    type: [String],
+  },
+  answers: 
+    [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Answer' }]
+    ,
+  
+  voteCount: {
+    type: Number,
+  },
+  postedOn: {
+    type: Date,
+  },
+  
+});
+
+const Question = mongoose.model("Question", questionSchema);
+module.exports = Question;
