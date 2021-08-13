5763c02f1bc1e07afc30e73f714a41e9c1be533d
ShalinJain1603 <shalin@iitg.ac.in>
2021-08-13 12:22:16 +0530

    Added navbar

--- client/src/router.js
+++ client/src/router.js
@@ -3,12 +3,12 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Auth from "./auth";
 import Private from "./components/private";
 import Public from "./components/public";
-import AddNewQuestion from './components/question/NewQuestionForm';
+import AddNewQuestion from "./components/question/NewQuestionForm";
 const Router = (props) => (
   <Switch>
     <Route exact path="/public" component={Public} />
     <Route exact path="/question/new" component={AddNewQuestion} />
-    <PrivateRoute path="/protected" component={Private} />
+    <PrivateRoute path="/private" component={Private} />
   </Switch>
 );
 
