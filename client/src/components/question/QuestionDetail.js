c53ba976d6087053e85dc2f9179c23a7301eecbd
ShalinJain1603 <shalin@iitg.ac.in>
2021-08-15 00:06:53 +0530

    Fixed the question detail page

--- client/src/components/question/QuestionDetail.js
+++ client/src/components/question/QuestionDetail.js
@@ -11,6 +11,7 @@ const QuestionDetail = (props) => {
     const fetchQuestion = async () => {
       const { data } = await axios.get(`/api/question/${questionId}`);
       setQuestion(data);
+      console.log(data);
     };
 
     fetchQuestion();
@@ -36,7 +37,6 @@ const QuestionDetail = (props) => {
       default:
         break;
     }
-
   };
   return (
     <Fragment>
@@ -71,60 +71,5 @@ const QuestionDetail = (props) => {
     </Fragment>
   );
 };
-=======
-    const sortByOldest = () => {
-        setAnswersSortType('Oldest');
-    }
-    const sortByVotes = () => {
-        setAnswersSortType('Votes');
-    }
-    const answerSorting = () => {
-        switch (answerSortType) {
-            case 'Votes':
-                return (a, b) => b.voteCount - a.voteCount
-            case 'Newest':
-                return (a, b) => new Date(b.postedOn) - new Date(a.postedOn)
-            case 'Oldest':
-                return (a, b) => new Date(a.postedOn) - new Date(b.postedOn)
-            default:
-                break
-        }
-    }
-    return <Fragment>
-        {!question && <p>Loading ...</p>}
-        {question &&
-
-            (<div>
-                <h1> {question.title} </h1>
-                {question.author.username}
-                <p> {question.text}</p>
-                {question.tags.map((tag) => {
-                    <h2> {tag}</h2>
-                })}
-            </div>)}
-        {
-            <div>
-                <h1> Sort Answers by</h1>
-                <button onClick={sortByOldest}> Oldest</button>
-                <button onClick={sortByNewest}> Newest</button>
-                <button onClick={sortByVotes}> Most Voted</button>
-            </div>
-        }
-        {question && question.answers.length && (question.answers.sort(answerSorting()).map((answer) => (
-            <div>
-                <h1> {answer.author.firstname}</h1>
-                <p>{answer.text}</p>
-                <h2> {answer.voteCount}</h2>
-            </div>
-        ))
-
-        )}
-
-
-
-
-    </Fragment>;
-}
-
 
 export default QuestionDetail;
