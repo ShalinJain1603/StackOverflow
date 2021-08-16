06157b2222d7b72cdcefb3170989784e58e8cb34
ShalinJain1603 <shalin@iitg.ac.in>
2021-08-16 22:11:15 +0530

    Added sidebar/offcanvas

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from "react";
+import { Offcanvas } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import {
   Collapse,
@@ -9,10 +10,14 @@ import {
   NavItem,
 } from "reactstrap";
 import Auth from "../auth";
+import classes from "./styles.module.css";
 
 const Header = (props) => {
   const [isNavOpen, setNavOpen] = useState(false);
   const [isloggedIn, setLogIn] = useState(false);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const login = () => {
     Auth.authenticate();
   };
@@ -33,9 +38,34 @@ const Header = (props) => {
   }, []);
   return (
     <div>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        scroll={true}
+        backdrop={false}
+        className={classes.offcanvas}
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>StackOverflow</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>Hello</Offcanvas.Body>
+      </Offcanvas>
       <Navbar dark expand="md" className="bg-dark ">
         <div className="container">
           <NavbarToggler onClick={toggleNav} />
+          <span onClick={handleShow} className="me-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="white"
+              class="bi bi-menu-button-wide"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v2A1.5 1.5 0 0 1 14.5 5h-13A1.5 1.5 0 0 1 0 3.5v-2zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-13z" />
+              <path d="M2 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm10.823.323-.396-.396A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2H1zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2h14zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+            </svg>
+          </span>
           <NavbarBrand href="/">StackOverflow</NavbarBrand>
           <Collapse isOpen={isNavOpen} navbar>
             <Nav navbar className="me-auto">
