698a74549dbf68fa0e8294272b4a6ed31197dc7e
ShalinJain1603 <shalin@iitg.ac.in>
2021-08-16 12:34:52 +0530

    Styled all question page

--- client/src/components/question/AllQuestions.js
+++ client/src/components/question/AllQuestions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { Fragment, useEffect, useState } from "react";
-
+import { Card, CardBody, CardTitle } from "reactstrap";
 const AllQuestions = () => {
   const [questions, setQuestions] = useState(null);
   const [sortBy, setSortBy] = useState("Newest");
@@ -41,27 +41,48 @@ const AllQuestions = () => {
 
   return (
     <Fragment>
-      <h1>All Questions Page </h1>
-      {!questions && <p> Loading...</p>}
-      {questions && (
-        <div>
-          <h2>Sort by</h2>
-          <button onClick={sortByVotesHandler}> Votes</button>
-          <button onClick={sortByNewestHandler}> Newest</button>
-          <button onClick={sortByOldestHandler}> Oldest</button>
-        </div>
-      )}
-      {questions &&
-        questions.sort(questionSorting()).map((question) => (
+      <div className="container">
+        <h1 className="display-3">All Questions Page </h1>
+        {!questions && <p> Loading...</p>}
+        {questions && (
           <div>
-            <h2> {question.title}</h2>
-            <h3> Posted by: {question.author.username}</h3>
-            <h3> Upvotes : {question.voteCount}</h3>
-            <p>{question.text}</p>
-            <h3> {question.answers.length} Replies</h3>
-            <hr />
+            <h5 className="h5">Sort by</h5>
+            <div className="btn-group" role="group">
+              <button onClick={sortByVotesHandler} className="btn btn-primary">
+                {" "}
+                Votes
+              </button>
+              <button onClick={sortByNewestHandler} className="btn btn-primary">
+                {" "}
+                Newest
+              </button>
+              <button onClick={sortByOldestHandler} className="btn btn-primary">
+                {" "}
+                Oldest
+              </button>
+            </div>
           </div>
-        ))}
+        )}
+        {questions &&
+          questions.sort(questionSorting()).map((question) => (
+            <div className="my-3">
+              <Card>
+                <CardBody>
+                  <CardTitle>
+                    <h5> {question.title}</h5>
+                  </CardTitle>
+                  <p className="text-muted">
+                    {" "}
+                    Posted by: {question.author.username}
+                  </p>
+                  <p> Upvotes : {question.voteCount}</p>
+                  <h5>{question.text}</h5>
+                  <p> {question.answers.length} Replies</p>
+                </CardBody>
+              </Card>
+            </div>
+          ))}
+      </div>
     </Fragment>
   );
 };
