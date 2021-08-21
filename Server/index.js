9044bea42a713e6b7576d098d330642d973683c7
Bhuyan-shamim <bhuyanshamim@gmail.com>
2021-08-21 17:43:54 +0530

    added helmet for security

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,9 @@ const passport = require("passport");
 const session = require("express-session");
 const OutlookStrategy = require("passport-outlook").Strategy;
 const path = require("path");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -37,6 +40,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(mongoSanitize({
+  replaceWith: '_'
+}))
+
+app.use(helmet({
+  contentSecurityPolicy: false
+}))
+
 // Passport-Outlook Config
 passport.serializeUser(function (user, done) {
   done(null, user);
